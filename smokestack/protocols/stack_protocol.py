90f16fee5fa07af2448cdc8c404c59856ddc9617
Cariad Eccleston <cariad@cariad.earth>
2021-12-10 09:40:54 +0000

    WIP

--- smokestack/protocols/stack_protocol.py
+++ smokestack/protocols/stack_protocol.py
@@ -1,8 +1,6 @@
-# from io import StringIO
 from pathlib import Path
 from typing import Protocol, Union
 
-from boto3.session import Session
 from cfp import StackParameters
 
 from smokestack.types.capability import Capabilities
@@ -11,20 +9,12 @@ from smokestack.types.capability import Capabilities
 class StackProtocol(Protocol):
     @property
     def body(self) -> Union[str, Path]:
-        """
-        Gets the template body or path to the template file.
-        """
+        """Gets the template body or path to the template file."""
         ...
 
     @property
     def capabilities(self) -> Capabilities:
         """Gets the capabilities required to deploy this stack."""
-
-        ...
-
-    @property
-    def exists(self) -> bool:
-        """Returns `True` if the stack exists, otherwise `False`."""
         ...
 
     @property
@@ -39,10 +29,6 @@ class StackProtocol(Protocol):
         ...
 
     @property
-    def session(self) -> Session:
-        """Gets this stack's Boto3 session."""
-        ...
-
-    @property
-    def stack_parameters(self) -> StackParameters:
+    def region(self) -> str:
+        """Gets the Amazon Web Services region to deploy this stack into."""
         ...
